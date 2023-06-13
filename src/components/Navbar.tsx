9b11a2fa0a7ed920a8247446c6797e1e9a0fe436
SUYEON KIM <yeonee230@gmail.com>
2023-06-13 16:52:39 +0900

    add header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
 import styled from 'styled-components'
+import { b, v } from '../styles/variables';
 
 export default function Navbar() {
     return (
-        <NavbarEl>
-            Navbar
-        </NavbarEl>
+        <>
+        <NavHeight />
+        <NavFixed>
+            <NavHeader>
+                <Left>left</Left>
+                <Center>center</Center>
+                <Right>right</Right>
+
+            </NavHeader>
+        </NavFixed>
+        </>
     );
 }
 
-const NavbarEl = styled.div``;
+const NavFixed = styled.div`
+position: fixed;
+right: 0;
+left: 0;
+top: 0;
+height: ${v.headerHeight};
+//background-color: ${({theme}) => theme.background};
+background-color: pink;
+z-index: 10;
+`;
+
+const NavHeight = styled.div`
+    height: ${v.headerHeight};
+`
+
+const NavHeader = styled.header`
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
+    max-width: 1920px;
+    display: flex;
+    align-items: center;
+
+    transition: 0.3s ease padding;
+    padding: 0 ${v.mdSpacing};
+
+    @media ${b.lg} {
+        padding: 0 ${v.lgSpacing}; 
+    }
+
+    > div {
+            flex: 1;
+        }
+`
+
+const Left = styled.div``
+const Center = styled.div`
+
+`
+const Right = styled.div``
